7beaf723122bef66b4d55d56ee42d34d290df3f0
Zakeria Ali <zak.fined@gmail.com>
2017-07-19 20:40:45 +0100

    Add transition to the individual panel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+var panels = document.querySelectorAll('.panel');
+    function toggleOpen() {
+      console.log('Hello');
+      this.classList.toggle('open');
+    }
+    function toggleActive(e) {
+      console.log(e.propertyName);
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    }
+    panels.forEach(function(panel){
+      panel.addEventListener('click', toggleOpen)
+    })
+    panels.forEach(function(panel){
+      panel.addEventListener('transitionend', toggleActive)
+    })
